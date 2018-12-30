8b02dead35f0d76e1280399bff1a1889f87cb6e9
Unknown <kelvin.tran@polymtl.ca>
2018-12-30 17:08:13 -0500

    Add mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,28 @@
 import { createServer } from 'http';
-import SocketIO from 'socket.io';
+import SocketIO, { Socket } from 'socket.io';
+import { MongoClient, MongoError } from 'mongodb';
 
 
 const httpServer = createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Brandon sucks dicks. multiple dicks!');
 }).listen(3000, '0.0.0.0');
+
 const ioServer =  SocketIO.listen(httpServer);
+const dbUrl = 'mongodb://localhost:27017/';
 
 console.log("Server started");
 
+MongoClient.connect(dbUrl, (err: MongoError, client: MongoClient) => {
+    if (err) {
+        throw err;
+    } else {
+        client.db('chatapp');
+    }
+});
 
 
-ioServer.on('connection', () => {
+ioServer.on('connection', (socket: Socket) => {
     console.log('user connected');
 });
 
